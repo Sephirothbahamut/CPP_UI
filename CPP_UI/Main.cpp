a88dea1c8a392621edac4af37a1b51e35545d77c
sephirothbahamut <sephirothbahamut@live.it>
2023-02-15 19:36:13 +0100

    made the main example less of a mess, added multiple windows to the example

--- CPP_UI/Main.cpp
+++ CPP_UI/Main.cpp
@@ -1,3 +1,5 @@
+﻿#include <thread>
+
 #include <utils/MS/window/window.h>
 #include <utils/MS/window/style.h>
 #include <utils/MS/window/regions.h>
@@ -8,48 +10,113 @@
 
 #include "UI/UI.h"
 
-UI::core::element_own title_bar(utils::MS::window::base& window, utils::MS::graphics::dw::factory dw_factory, utils::MS::graphics::dw::text_format format, utils::MS::graphics::d2d::solid_brush brush, const std::wstring& string)
+struct style
 	{
-	auto root{std::make_unique<UI::containers::stack>()};
+	utils::MS::graphics::dw::factory dw_factory;
+
+	struct text_t
+		{
+		utils::MS::graphics::dw::text_format format;
+		utils::MS::graphics::d2d::brush      brush;
+		} text;
+
+	struct button_t
+		{
+		UI::drawables::draw_shape_data normal;
+		UI::drawables::draw_shape_data hover;
+		UI::drawables::draw_shape_data down;
+		text_t text;
+		} button;
+	};
+
+std::unique_ptr<UI::widgets::button> make_button(const style& style, const std::wstring& text, std::function<void()> callback)
+	{
+	auto make_layer{[&](const UI::drawables::draw_shape_data& draw_shape_data)
+		{
+		auto root{std::make_unique<UI::containers::overlay>()};
+		root->align_hor = UI::core::align_hor::center;
+		root->align_ver = UI::core::align_ver::middle;
+
+		if (auto & rect{root->emplace<UI::drawables::rect>()})
+			{
+			rect.draw_shape_data = draw_shape_data;
+			}
+		root->emplace<UI::drawables::text>(style.dw_factory, style.button.text.format, style.button.text.brush, text);
+		return root;
+		}};
+
+	auto root{std::make_unique<UI::widgets::button>(callback)};
+	root->align_hor = UI::core::align_hor::center;
+	root->align_ver = UI::core::align_ver::middle;
+	root->sizes.min_x = 128.f;
+	root->sizes.min_y =  24.f;
+	root->sizes.prf_x = 128.f;
+	root->sizes.prf_y =  24.f;
+	root->sizes.max_x = 256.f;
+	root->sizes.max_y =  32.f;
+
+	root->push(make_layer(style.button.normal));
+	root->push(make_layer(style.button.hover ));
+	root->push(make_layer(style.button.down  ));
+
+	return root;
+	}
+
+UI::core::element_own title_bar(utils::MS::window::base& window, const style& style, const std::wstring& string)
+	{
+	auto root{std::make_unique<UI::containers::overlay>()};
+	root->sizes.max_y = 24;
+	root->sizes.min_y = 24;
 
 	if (auto& drag_layer{root->emplace<UI::widgets::window_drag>(window)}) 
 		{
 		drag_layer.sizes.max_x = std::numeric_limits<float>::infinity();
 		drag_layer.sizes.max_y = std::numeric_limits<float>::infinity();
 		}
+
 	if (auto& buttons_bar{root->emplace<UI::containers::group_hor>()})
 		{
-		buttons_bar.emplace<UI::drawables::text>(dw_factory, format, brush, string);
-		buttons_bar.emplace<UI::widgets::spacer>();
-		if (auto & button{buttons_bar.emplace<UI::widgets::button>([&window]() { window.minimize(); })})
+		buttons_bar.alignment = UI::core::align_ver::middle;
+		if (auto& text{buttons_bar.emplace<UI::drawables::text>(style.dw_factory, style.text.format, style.text.brush, string)})
 			{
-			button.sizes.max_x = 32;
-			button.sizes.min_x = 32;
+			text.sizes.min_x = 0.f;
+			text.sizes.prf_x = 1;
+			text.sizes.max_x = UI::core::finf;
 			}
-		if (auto & button{buttons_bar.emplace<UI::widgets::button>([&window]() { window.maximize(); })})
+
+		//buttons_bar.emplace<UI::widgets::spacer>();
+		if (auto btn{make_button(style, L"–", [&window]() {window.minimize(); })})
 			{
-			button.sizes.max_x = 32;
-			button.sizes.min_x = 32;
+			btn->sizes.min_x = 32.f;
+			btn->sizes.prf_x = 32.f;
+			btn->sizes.max_x = 32.f;
+			buttons_bar.push(std::move(btn));
 			}
-		if (auto & button{buttons_bar.emplace<UI::widgets::button>([&window]() { window.close(); })})
+		if (auto btn{make_button(style, L"🗔", [&window]() { if (window.is_maximized()) { window.restore(); } else { window.maximize(); } })})
 			{
-			button.sizes.max_x = 32;
-			button.sizes.min_x = 32;
+			btn->sizes.min_x = 32.f;
+			btn->sizes.prf_x = 32.f;
+			btn->sizes.max_x = 32.f;
+			buttons_bar.push(std::move(btn));
+			}
+		if (auto btn{make_button(style, L"❌", [&window]() {window.close(); })})
+			{
+			btn->sizes.min_x = 32.f;
+			btn->sizes.prf_x = 32.f;
+			btn->sizes.max_x = 32.f;
+			buttons_bar.push(std::move(btn));
 			}
 		}
-
-	root->sizes.max_y = 24;
-	root->sizes.min_y = 24;
 
 	return std::move(root);
 	}
 
-UI::core::element_own sample_ui(utils::MS::window::base& window, utils::MS::graphics::dw::factory dw_factory, utils::MS::graphics::dw::text_format format, utils::MS::graphics::d2d::solid_brush brush, const std::wstring& string)
+UI::core::element_own sample_ui(utils::MS::window::base& window, const style& style, const std::wstring& string)
 	{
 	auto root{std::make_unique<UI::containers::group_ver>()};
 	root->alignment = UI::align_hor::left;
 
-	root->push(title_bar(window, dw_factory, format, brush, string));
+	root->push(title_bar(window, style, string));
 
 	if (auto & top{root->emplace<UI::containers::group_hor>()})
 		{
@@ -109,8 +176,9 @@ UI::core::element_own sample_ui(utils::MS::window::base& window, utils::MS::grap
 			}
 		}
 
-	//generator<group_hor> root{};
-	//auto&& bottom{root};
+	//auto root{std::make_unique<UI::containers::group_hor>()};
+	//root->alignment = UI::align_ver::top;
+	//auto& bottom{*root};
 	if (auto & bottom{root->emplace<UI::containers::group_hor>()})
 		{
 		bottom.alignment = UI::align_ver::top;
@@ -121,8 +189,8 @@ UI::core::element_own sample_ui(utils::MS::window::base& window, utils::MS::grap
 			if (auto & element{padding.emplace<UI::widgets::dummy>()})
 				{
 				element.sizes.min_x = 10;
-				element.sizes.prf_x = 64;
-				element.sizes.max_x = 128;
+				element.sizes.prf_x = 128;
+				element.sizes.max_x = 256;
 				}
 			}
 
@@ -133,16 +201,21 @@ UI::core::element_own sample_ui(utils::MS::window::base& window, utils::MS::grap
 			if (auto & element{padding.emplace<UI::widgets::dummy>()})
 				{
 				element.sizes.min_x = 10;
-				element.sizes.prf_x = 64;
-				element.sizes.max_x = 128;
+				element.sizes.prf_x = 128;
+				element.sizes.max_x = 256;
 				}
 			}
 		}
 
-	if (auto & sp{root->emplace<UI::widgets::spacer>()})
+	//if (auto & body{root->emplace<UI::containers::group_ver>()})
 		{
-		//sp.sizes.max_y = 32.f;
+		auto& body{*root};
+		body.push(make_button(style, L"Hello world!", []() {std::cout << "Hello world!" << std::endl; }));
+		body.push(make_button(style, L"Hello cats! ", []() {std::cout << "Hello cats! " << std::endl; }));
+		body.push(make_button(style, L"Hello dogs! ", []() {std::cout << "Hello dogs! " << std::endl; }));
 		}
+
+	root->emplace<UI::widgets::spacer>();
 
 	return std::move(root);
 	}
@@ -182,7 +255,7 @@ int main()
 		std::cout << "(" << reinterpret_cast<uintptr_t>(device.hDevice) << ")" << std::endl;
 		}
 
-
+	utils::MS::window::initializer window_initializer;
 
 	// Setup rendering
 	utils::MS::graphics::co  ::initializer    co_initializer;
@@ -194,130 +267,219 @@ int main()
 	utils::MS::graphics::dw  ::factory        dw_factory;
 	utils::MS::graphics::composition::device  composition_device{dxgi_device};
 	UI::initializer ui_initializer{d2d_context};
+	//UI resources
 
-	utils::containers::object_pool<std::function<void(const utils::MS::graphics::d2d::device_context&)>> draw_operations;
-
-	// Setup window 
-	utils::MS::window::initializer window_initializer;
+	utils::MS::window::base window_a{utils::MS::window::base::create_info{.title{L"George G. Goof"}}};
+	utils::MS::window::base window_b{utils::MS::window::base::create_info{.title{L"George G. Goof"}}};
 	
-	utils::MS::window::base window
+	utils::MS::graphics::d2d::device_context a_d2d_device_context{d2d_device};
+	utils::MS::graphics::composition::device a_composition_device{d2d_device.get_dxgi_device()};
+	utils::MS::graphics::composition::visual a_composition_visual{composition_device};
+	utils::MS::graphics::composition::target a_composition_target{composition_device, window_a.get_handle()};
+	utils::MS::graphics::dxgi::swap_chain    a_dxgi_swapchain{d2d_device.get_dxgi_device(), window_a.get_handle(), nullptr};
+	
+	utils::MS::graphics::d2d::device_context b_d2d_device_context{d2d_device};
+	utils::MS::graphics::composition::device b_composition_device{d2d_device.get_dxgi_device()};
+	utils::MS::graphics::composition::visual b_composition_visual{composition_device};
+	utils::MS::graphics::composition::target b_composition_target{composition_device, window_b.get_handle()};
+	utils::MS::graphics::dxgi::swap_chain    b_dxgi_swapchain{d2d_device.get_dxgi_device(), window_b.get_handle(), nullptr};
+
+	style style
 		{
-		utils::MS::window::base::create_info
+		.dw_factory{dw_factory},
+		.text
 			{
-			.title{L"George G. Goof"},
-			//.position{{10, 60}},
-			//.size{{800, 600}}
-			},
-		utils::MS::window::style::create_info
-			{
-			.transparency{utils::MS::window::style::transparency_t::composition_attribute},
-			.borders     {utils::MS::window::style::value_t::disable}
-			},
-		utils::MS::window::resizable_edge::create_info
-			{
-			.thickness{8}
-			},
-		//utils::MS::graphics::d2d::window::composition_swap_chain::create_info
-		//	{
-		//	.d2d_device{d2d_device},
-		//	.on_render{[&](utils::MS::window::base& base, const utils::MS::graphics::d2d::device_context& context)
-		//		{
-		//		context->SetTransform(D2D1::IdentityMatrix());
-		//		context->Clear(D2D1_COLOR_F{0.f, 0.f, 0.f, 0.f});
-		//
-		//		for (const auto& draw_operation : draw_operations)
-		//			{
-		//			draw_operation(context);
-		//			}
-		//		}}
-		//	},
-		utils::MS::graphics::d2d::window::render_target::create_info
-			{
-			.d2d_factory{d2d_factory},
-			.on_render{[&](utils::MS::window::base& base, const utils::MS::graphics::d2d::device_context& context)
+			.format
 				{
-				context->SetTransform(D2D1::IdentityMatrix());
-				context->Clear(D2D1_COLOR_F{0.f, 0.f, 0.f, 0.f});
-		
-				for (const auto& draw_operation : draw_operations)
+				dw_factory,
+				utils::MS::graphics::dw::text_format::create_info
 					{
-					draw_operation(context);
+					.name{L"Arial"},
+					//.name{L"Gabriola"},
+					.size{16},
+					.alignment_ver{utils::MS::graphics::dw::text_format::alignment_ver::center},
+					.locale{L"en-gb"},
 					}
-				}}
+				},
+			.brush{utils::MS::graphics::d2d::solid_brush{d2d_context, utils::graphics::colour::rgba_f{0.9f, 0.9f, 0.9f, 1.f}}.as<utils::MS::graphics::d2d::brush>()}
 			},
-		//utils::MS::graphics::d2d::window::swap_chain::create_info
-		//	{
-		//	.d2d_device{d2d_device},
-		//	.on_render{[&](utils::MS::window::base& base, const utils::MS::graphics::d2d::device_context& context)
-		//		{
-		//		context->SetTransform(D2D1::IdentityMatrix());
-		//		context->Clear(D2D1_COLOR_F{0.f, 0.f, 0.f, 0.f});
-		//
-		//		for (const auto& draw_operation : draw_operations)
-		//			{
-		//			draw_operation(context);
-		//			}
-		//		}}
-		//	},
-		utils::MS::window::input::mouse::create_info{}
+		.button
+			{
+			.normal
+				{
+				.brush_fill_opt   {utils::MS::graphics::d2d::solid_brush{d2d_context, utils::graphics::colour::rgba_f{.6f, 0.6f, 0.6f, 1.f}}.as<utils::MS::graphics::d2d::brush>()},
+				.brush_outline_opt{utils::MS::graphics::d2d::solid_brush{d2d_context, utils::graphics::colour::rgba_f{.5f, 0.5f, 0.5f, 1.f}}.as<utils::MS::graphics::d2d::brush>()},
+				.outline_thickness{2.f}
+				},
+			.hover
+				{
+				.brush_fill_opt   {utils::MS::graphics::d2d::solid_brush{d2d_context, utils::graphics::colour::rgba_f{.7f, 0.7f, 0.7f, 1.f}}.as<utils::MS::graphics::d2d::brush>()},
+				.brush_outline_opt{utils::MS::graphics::d2d::solid_brush{d2d_context, utils::graphics::colour::rgba_f{.6f, 0.6f, 0.6f, 1.f}}.as<utils::MS::graphics::d2d::brush>()},
+				.outline_thickness{2.f}
+				},
+			.down
+				{
+				.brush_fill_opt   {utils::MS::graphics::d2d::solid_brush{d2d_context, utils::graphics::colour::rgba_f{.3f, 0.3f, 0.3f, 1.f}}.as<utils::MS::graphics::d2d::brush>()},
+				.brush_outline_opt{utils::MS::graphics::d2d::solid_brush{d2d_context, utils::graphics::colour::rgba_f{.4f, 0.4f, 0.4f, 1.f}}.as<utils::MS::graphics::d2d::brush>()},
+				.outline_thickness{2.f}
+				},
+			.text
+				{
+				.format
+					{
+					dw_factory,
+					utils::MS::graphics::dw::text_format::create_info
+						{
+						.name{L"Arial"},
+						//.name{L"Gabriola"},
+						.size{16},
+						.alignment_hor{utils::MS::graphics::dw::text_format::alignment_hor::center},
+						.alignment_ver{utils::MS::graphics::dw::text_format::alignment_ver::center},
+						.locale{L"en-gb"},
+						}
+					},
+				.brush{utils::MS::graphics::d2d::solid_brush{d2d_context, utils::graphics::colour::rgba_f{0.9f, 0.9f, 0.9f, 1.f}}.as<utils::MS::graphics::d2d::brush>()}
+				}
+			}
 		};
 
-	auto& default_mouse{window.get_module_ptr<utils::MS::window::input::mouse>()->default_mouse};
-
-	//UI resources
-	utils::MS::graphics::dw::text_format font_format{dw_factory, utils::MS::graphics::dw::text_format::create_info
+	DWRITE_TRIMMING trimming
 		{
-		.name{L"Gabriola"},
-		.size{16},
-		.locale{L"en-gb"}
+		.granularity   {DWRITE_TRIMMING_GRANULARITY_CHARACTER},
+		//.delimiter     {static_cast<uint32_t>(L'…')}, //0x2026
+		//.delimiterCount{1}
+		//.delimiter     {static_cast<uint32_t>(L'.')}, //0x00B7
+		//.delimiterCount{3}
+		.delimiter     {0x00B7},
+		.delimiterCount{3}
+		};
+	style.text.format->SetTrimming(&trimming, nullptr);
+
+	auto spawn_window{[&]()
+		{
+		utils::containers::object_pool<std::function<void(const utils::MS::graphics::d2d::device_context&)>> draw_operations;
+
+		// Setup window 
+	
+		utils::MS::window::base window
+			{
+			utils::MS::window::base::create_info
+				{
+				.title{L"George G. Goof"},
+				//.position{{10, 60}},
+				//.size{{800, 600}}
+				},
+			utils::MS::window::style::create_info
+				{
+				.transparency{utils::MS::window::style::transparency_t::composition_attribute},
+				.borders     {utils::MS::window::style::value_t::disable}
+				},
+			utils::MS::window::resizable_edge::create_info
+				{
+				.thickness{8}
+				},
+			//utils::MS::graphics::d2d::window::render_target::create_info
+			//utils::MS::graphics::d2d::window::swap_chain::create_info
+			utils::MS::graphics::d2d::window::composition_swap_chain::create_info
+				{
+				//.d2d_factory{d2d_factory},
+				.d2d_device{d2d_device},
+				.draw_callback{[&](const utils::MS::window::base& base, const utils::MS::graphics::d2d::device_context& context)
+					{
+					context->SetTransform(D2D1::IdentityMatrix());
+					context->Clear(D2D1_COLOR_F{0.f, 0.f, 0.f, 0.f});
+		
+					for (const auto& draw_operation : draw_operations)
+						{
+						draw_operation(context);
+						}
+					}}
+				},
+			utils::MS::window::input::mouse::create_info{}
+			};
+
+		auto& default_mouse{window.get_module_ptr<utils::MS::window::input::mouse>()->default_mouse};
+
+		UI::manager ui_manager{ui_initializer, sample_ui(window, style, L"Donald Fauntleroy Duck"), default_mouse};
+
+		auto& window_ui{window.emplace_module_from_create_info(UI::window::create_info{.manager_ptr{&ui_manager}})};
+
+		// Background grid
+		draw_operations.emplace([&window](const utils::MS::graphics::d2d::device_context& context)
+			{
+			// Draw a grid background.
+			utils::MS::graphics::d2d::solid_brush brush{context, utils::graphics::colour::rgba_f{0.f, 1.f, 0.f, 0.1f}};
+		
+			utils::math::vec2f sizef{static_cast<utils::math::vec2l>(window.client_rect.size())};
+		
+			for (float x = 0.f; x < sizef.x; x += 10.f)
+				{
+				context->DrawLine
+					(
+					D2D1::Point2F(x, 0.0f   ),
+					D2D1::Point2F(x, sizef.y),
+					brush.get(),
+					0.5f
+					);
+				}
+			for (float y = 0.f; y < sizef.y; y += 10.f)
+				{
+				context->DrawLine
+					(
+					D2D1::Point2F(0.0f   , y),
+					D2D1::Point2F(sizef.x, y),
+					brush.get(),
+					0.5f
+					);
+				}
+			});
+
+		//Setup UI
+
+		draw_operations.emplace([&ui_manager](const utils::MS::graphics::d2d::device_context& context)
+			{
+			ui_manager.debug_draw(context);
+			ui_manager.draw(context);
+			});
+
+		//return window;
+
+		window.show();
+		while (window.is_open())
+			{
+			window.wait_event();
+			}
+
+		return window;
 		}};
-	utils::MS::graphics::d2d::solid_brush font_color{d2d_context, utils::graphics::colour::rgba_f{0.f, 0.f, 0.f, 1.f}};
 
-	UI::manager ui_manager{ui_initializer, sample_ui(window, dw_factory, font_format, font_color, L"Donald Fauntleroy Duck"), default_mouse};
-
-	auto& window_ui{window.emplace_module_from_create_info(UI::window::create_info{.manager_ptr{&ui_manager}})};
-
-	// Background grid
-	draw_operations.emplace([&window](const utils::MS::graphics::d2d::device_context& context)
+	std::vector<std::jthread> threads;
+	threads.emplace_back([&spawn_window]()
 		{
-		// Draw a grid background.
-		utils::MS::graphics::d2d::solid_brush brush{context, utils::graphics::colour::rgba_f{0.f, 1.f, 0.f, 1.f}};
-		
-		utils::math::vec2f sizef{static_cast<utils::math::vec2l>(window.client_rect.size())};
-		
-		for (float x = 0.f; x < sizef.x; x += 10.f)
+		try
 			{
-			context->DrawLine
-				(
-				D2D1::Point2F(x, 0.0f   ),
-				D2D1::Point2F(x, sizef.y),
-				brush.get(),
-				0.5f
-				);
+			auto window{spawn_window()};
+
+			window.show();
+			while (window.is_open())
+				{
+				window.wait_event();
+				}
 			}
-		for (float y = 0.f; y < sizef.y; y += 10.f)
+		catch (const std::exception& e) { std::cout << e.what() << std::endl; }
+		});
+	threads.emplace_back([&spawn_window]()
+		{
+		try
 			{
-			context->DrawLine
-				(
-				D2D1::Point2F(0.0f   , y),
-				D2D1::Point2F(sizef.x, y),
-				brush.get(),
-				0.5f
-				);
+			auto window{spawn_window()};
+
+			window.show();
+			while (window.is_open())
+				{
+				window.wait_event();
+				}
 			}
+		catch (const std::exception& e) { std::cout << e.what() << std::endl; }
 		});
-
-	//Setup UI
-
-	draw_operations.emplace([&ui_manager](const utils::MS::graphics::d2d::device_context& context)
-		{
-		ui_manager.debug_draw(context);
-		ui_manager.draw(context);
-		});
-
-	window.show();
-	while (window.is_open())
-		{
-		window.wait_event();
-		}
 	}
